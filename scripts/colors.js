bb8c11ae42c5276607926cd59f02ce2666dd4488
jamestroxel <james.r.troxel@gmail.com>
2020-11-27 09:03:03 -0500

    plotted

--- scripts/colors.js
+++ scripts/colors.js
@@ -1,109 +1,94 @@
-d3.json('data/colorCats.json').then(function(data){
-  
-  var margin = ({top: -150, right: 0, bottom: 0, left: 0})
-  width = 2000 - margin.left - margin.right,
-  height = 800 - margin.top - margin.bottom;
+d3.json('data/data.json').then(function(data){
+  var width = 2000;
+  var height = 1000;
 
   var tooltip = d3.select("#toolTip")
   .append("div")
   .style("color", "white");
 
-  // vertices = function(d){return [d.latitude*width,d.longitude*height]}; 
-  vertices = d3.range(100).map(function(d){ 
-    return [Math.random()*width,Math.random()*height]; 
-  })
+  const xScale = d3.scaleLinear()
+  .domain([-90, 90])
+  .range([0, width])
+  const yScale = d3.scaleLinear()
+  .domain([-180, 180])
+  .range([0, height]); 
 
-  // points = [];
-  // function makePoints(){
-  //   data.forEach(function(d) {
-  //   points.push(d.latitude)})}
-  
-  // makePoints();
-
-    // d.latitude, d.longitude]
-  voronoi = d3.voronoi().size([width,height]);
   var svg = d3.select('#viz')
-    .append('svg')
-    .attr("fill", "white")
-    .attr("width", "100%")
-    .attr("height", "100%");
-    
-    svg.append("g")
-    .attr("class", "polygons")
-    .selectAll("path")
-    .data(voronoi.polygons(vertices))
-    .enter().append("path")
-    .attr("d", function(d){return "M"+d.join("L")+"Z"});
-    
-    svg.append("g")
-    .selectAll("circle")
-    
-    .data(vertices)
-    .enter().append("circle")
-    .attr("cx", function(d){return d[0]; })
-    .attr("cy", function(d){return d[1]; })
-    .attr("r", "2.5")
-    .attr("fill", "black")
-    .on("mouseover", function(event, d) {
-      tooltip
-      .html("")
-      .append("div")
-      .attr('class', 'toolTipData')
-      .selectAll("text")
-      .data(data)
-      .join("text")
-      .html(function(d) { return  "Name: " + "<b>" + d.title + "</b>" + "<br/>" 
-      + "Carat Weight: " + "<b>" + d.caratWeight + "</b>" + "<br/>" 
-      + "Description: " + "<b>" + d.lowercaseName + "</b>" + "<br/>" 
-      + "<a href=" + d.link + ' target="_blank"' + "><b>Link</b></a>" + "<br/>" + "Photo:"; })
-      .append('img')
-      .attr('class', 'toolTip')
-      .attr('width', 340)
-      .attr('height', 100)
-      .attr('src', function(d) { 
-        if (d.filename === "NMNH-501014.jpg" 
-        || d.filename === "NMNH-503014.jpg" 
-        || d.filename === "NMNH-504077.jpg"
-        || d.filename === "NMNH-NMNH-MS-2018-00018.jpg" 
-        || d.filename === "NMNH-NMNH-MS-2018-00019_screen.jpg"
-        || d.filename === "NMNH-504085.jpg"
-        || d.filename === "not found"){  
-          return "downSamples/JPEG/placeholder-01.svg";
-        } else {return 'downSamples/JPEG/' + d.filename; }})
-        return tooltip.style("visibility", "visible") 
-    })
-    .on("click", function(event, d) {
-      tooltip
-      .html("")
-      .append("div")
-      .attr('class', 'toolTipData')
-      .selectAll("text")
-      .data(data)
-      .join("text")
-      .html(function(d) { return  "Name: " + "<b>" + d.title + "</b>" + "<br/>" 
-      + "Carat Weight: " + "<b>" + d.caratWeight + "</b>" + "<br/>" 
-      + "Description: " + "<b>" + d.lowercaseName + "</b>" + "<br/>" 
-      + "<a href=" + d.link + ' target="_blank"' + "><b>Link</b></a>" + "<br/>" + "Photo:"; })
-      .append('img')
-      .attr('class', 'toolTip')
-      .attr('width', 340)
-      .attr('height', 100)
-      .attr('src', function(d) {
-        if (d.filename === "NMNH-501014.jpg" 
-        || d.filename === "NMNH-503014.jpg" 
-        || d.filename === "NMNH-504077.jpg"
-        || d.filename === "NMNH-NMNH-MS-2018-00018.jpg" 
-        || d.filename === "NMNH-NMNH-MS-2018-00019_screen.jpg"
-        || d.filename === "NMNH-504085.jpg"
-        || d.filename === "not found"){  
-          return "downSamples/JPEG/placeholder-01.svg";
-        } else {return 'downSamples/JPEG/' + d.filename; }})
-      return tooltip.style("visibility", "visible") 
-    });
+  .append('svg')
+  .attr('class', 'map')
+  .attr('width', width)
+  .attr('height', height);
+
+  svg.selectAll('g')
+  .data(data)
+  .join('circle')
+  .attr('fill', 'white')
+  .attr('cx', d => xScale(d.geometry.coordinates[1]))
+  .attr('cy', d => yScale(d.geometry.coordinates[0]))
+  .attr('r', '5')
+  .style('cursor', 'cell')
+  // .on("mouseover", function(event, d) {
+  //   tooltip
+  //   .html("")
+  //   .append("div")
+  //   .attr('class', 'toolTipData')
+  //   .select("text")
+  //   .data(data)
+  //   .join("text")
+  //   .html(function(d) { return  "Name: " + "<b>" + d.title + "</b>" + "<br/>" 
+  //   + "Carat Weight: " + "<b>" + d.caratWeight + "</b>" + "<br/>" 
+  //   + "Description: " + "<b>" + d.lowercaseName + "</b>" + "<br/>" 
+  //   + "<a href=" + d.link + ' target="_blank"' + "><b>Link</b></a>" + "<br/>" + "Photo:"; })
+  //   .append('img')
+  //   .attr('class', 'toolTip')
+  //   .style('color', 'red')
+  //   .attr('width', 340)
+  //   .attr('height', 100)
+  //   .attr('src', function(d) { 
+  //     if (d.filename === "NMNH-501014.jpg" 
+  //     || d.filename === "NMNH-503014.jpg" 
+  //     || d.filename === "NMNH-504077.jpg"
+  //     || d.filename === "NMNH-NMNH-MS-2018-00018.jpg" 
+  //     || d.filename === "NMNH-NMNH-MS-2018-00019_screen.jpg"
+  //     || d.filename === "NMNH-504085.jpg"
+  //     || d.filename === "not found.jpg"){  
+  //       return 'downSamples/JPEG/placeholder-01.svg';
+  //     } else {return 'downSamples/JPEG/' + d.filename; }})
+  //     return tooltip.style("visibility", "visible") 
+  // })
+  .on("click", function(event, d) {
+    tooltip
+    .html("")
+    .append("div")
+    .attr('class', 'toolTipData')
+    .selectAll("text")
+    .data(data)
+    .join("text")
+    .html(function(d) { return  "Name: " + "<b>" + d.title + "</b>" + "<br/>" 
+    + "Carat Weight: " + "<b>" + d.caratWeight + "</b>" + "<br/>" 
+    + "Description: " + "<b>" + d.lowercaseName + "</b>" + "<br/>" 
+    + "<a href=" + d.link + ' target="_blank"' + "><b>Link</b></a>" + "<br/>" + "Photo:"; })
+    .append('img')
+    .attr('class', 'toolTip')
+    .attr('width', 340)
+    .attr('height', 100)
+    .attr('src', function(d) {
+      if (d.filename === "NMNH-501014.jpg" 
+      || d.filename === "NMNH-503014.jpg" 
+      || d.filename === "NMNH-504077.jpg"
+      || d.filename === "NMNH-NMNH-MS-2018-00018.jpg" 
+      || d.filename === "NMNH-NMNH-MS-2018-00019_screen.jpg"
+      || d.filename === "NMNH-504085.jpg"
+      || d.filename === "not found.jpg"){  
+        return "downSamples/JPEG/placeholder-01.svg";
+      } else {return 'downSamples/JPEG/' + d.filename; }})
+    return tooltip.style("visibility", "visible") 
+  })
+});
 
 
-  ///// Dashboard Carat Chart /////////
-  // let bins;    
+///// Dashboard Carat Chart /////////
+  let bins;    
   var caratWeights =[];
   var colors =[];
   d3.json('data/data.json').then(function(data){ 
@@ -151,7 +136,7 @@ d3.json('data/colorCats.json').then(function(data){
       svg.append("line")
         .each(function(d) {this._current = d;} )
   });
-});
+
 ///////// Dashboard Color Chart ////////
 d3.json('data/colorCats.json').then(function(data){
   
@@ -182,3 +167,7 @@ d3.json('data/colorCats.json').then(function(data){
     .attr("y", d => y(d.value))
     .attr("height", d => y(0) - y(d.value) + 35)
   });
+
+
+
+
